68ba36182feae1f05e5b2b8e0efcdc23cc57da1b
jean davy Nizigama <jedavy.n@gmail.com>
2022-11-10 16:03:46 +0200

    Finished working on the registration feature

--- src/views/auth/register.vue
+++ src/views/auth/register.vue
@@ -4,39 +4,87 @@
         <form id="user">
             <div class="field">
                 <div class="control">
-                    <input class="input" type="text" placeholder="Names">
+                    <input class="input" v-model="names" type="text" placeholder="Names">
                 </div>
             </div>
             <div class="field">
                 <div class="control">
-                    <input class="input" type="email" placeholder="Email">
-                </div>
-            </div>
-
-            <div class="field">
-                <div class="control">
-                    <input class="input" type="password" placeholder="Password">
+                    <input class="input" v-model="email" type="email" placeholder="Email">
                 </div>
             </div>
 
             <div class="field">
                 <div class="control">
-                    <input class="input" type="password" placeholder="Password confirmation">
+                    <input class="input" v-model="password" type="password" placeholder="Password">
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <input class="input" v-model="password_confirmation" type="password"
+                        placeholder="Password confirmation">
                 </div>
             </div>
 
             <br>
 
-            <button type="submit" class="button is-black">Register</button>
+            <button @click.prevent="register" class="button is-black" :disabled="registering">
+                {{ registering ? 'Processing ...' : 'Register' }}
+            </button>
             &nbsp;
-            <button type="submit" class="button">Login</button>
+            <router-link :to="{ name: 'Login' }" class="button">Login</router-link>
         </form>
     </div>
 </template>
 
 <script>
+import { axiosErrorHandlingMixin } from '../../mixins/axiosErrorHandling'
+import { authCheck } from '../../mixins/authentication'
 export default {
+    data() {
+        return {
+            names: null,
+            email: null,
+            password: null,
+            password_confirmation: null,
+            registering: false,
+        }
+    },
+    mixins: [axiosErrorHandlingMixin, authCheck],
+    methods: {
+        async register() {
+            this.registering = true
+            try {
+                const response = await axios.post("/auth/register", {
+                    names: this.names,
+                    email: this.email,
+                    password: this.password,
+                    password_confirmation: this.password_confirmation
+                })
+                this.registering = false
 
+                this.saveAuthToken(response.data.token)
+
+                toast.fire("Info", "Registration successful", "success");
+
+                this.$router.push({ name: 'home' })
+            } catch (error) {
+                this.registering = false
+                this.handleAxiosNetworkError(error)
+            }
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        const token = localStorage.getItem("auth_token")
+
+        if (token !== null) {
+            next(vm => vm.$router.push({ name: 'Home' }))
+            return
+        }
+        next()
+    },
+    mounted() {
+    }
 }
 </script>
 
