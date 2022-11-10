6a60240fc504e98110ef42a19a86e18736adf86c
jean davy Nizigama <jedavy.n@gmail.com>
2022-11-10 12:30:31 +0200

    Added buttons that will be used to control authentication status

--- src/views/catalog/index.vue
+++ src/views/catalog/index.vue
@@ -1,3 +1,21 @@
+<template>
+  <div class="px-6">
+    <div style="float: right">
+      <button type="submit" class="button is-black">Register</button>
+      &nbsp;
+      <button type="submit" class="button">Login</button>
+      &nbsp;
+      <button type="submit" class="button">Logout</button>
+    </div>
+    <br/>
+    <h2 class="label">🎁 Products</h2>
+    <div id="products" class="columns p-4">
+
+      <Product v-for="(px, k) in products" :details="px" :key="k" />
+    </div>
+  </div>
+</template>
+
 <script>
 import Product from "../../components/catalog/Product.vue"
 export default {
@@ -27,21 +45,16 @@ export default {
           imageLink: "https://via.placeholder.com/150",
           price: 820
         },
+        {
+          name: "Product 2",
+          imageLink: "https://via.placeholder.com/150",
+          price: 820
+        },
       ]
     }
   }
 }
 </script>
-
-<template>
-  <div>
-    <h2 class="label ml-6">🎁 Products</h2>
-    <div id="products" class="columns p-4">
-
-      <Product v-for="(px, k) in products" :details="px" :key="k" />
-    </div>
-  </div>
-</template>
 
 <style scoped>
 
