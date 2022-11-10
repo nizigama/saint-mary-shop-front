e9e00ed204f9caf552322aae478e279e6e9ea6b8
jean davy Nizigama <jedavy.n@gmail.com>
2022-11-10 13:24:13 +0200

    Installed and cconfigured axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
 import './assets/main.css'
+
+axios.defaults.baseURL = import.meta.env.APP_API_URL
+
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.get['Accept'] = 'application/json';
 
 const app = createApp(App)
 
