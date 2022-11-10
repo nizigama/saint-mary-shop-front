f552ca5c8393c8033d20fa2af2fbc96f8a6831aa
jean davy Nizigama <jedavy.n@gmail.com>
2022-11-10 15:55:25 +0200

    Installed an notification package Fixed axios baseUrl import configured the notification package and added axios to the window object

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@ import App from './App.vue'
 import router from './router'
 
 import axios from 'axios'
+import swal from "sweetalert2";
 
 import './assets/main.css'
 
-axios.defaults.baseURL = import.meta.env.APP_API_URL
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.get['Accept'] = 'application/json';
+
+const toast = swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 8000,
+});
+
+window.axios = axios;
+window.swal = swal;
+window.toast = toast;
 
 const app = createApp(App)
 
