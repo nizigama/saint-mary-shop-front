214b2b52003cb7ae835c35a370d164387168e6c5
jean davy Nizigama <jedavy.n@gmail.com>
2022-11-10 16:03:56 +0200

    House cleaning and small improvements

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Catalog from '../views/catalog/index.vue'
-import Login from '../views/auth/login.vue'
-import Register from '../views/auth/register.vue'
-import Cart from '../views/cart/index.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Catalog from "../views/catalog/index.vue";
+import Login from "../views/auth/login.vue";
+import Register from "../views/auth/register.vue";
+import Cart from "../views/cart/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Catalog
+      path: "/",
+      name: "Home",
+      component: Catalog,
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: Login
+      path: "/login",
+      name: "Login",
+      component: Login,
     },
     {
-      path: '/register',
-      name: 'Register',
-      component: Register
+      path: "/register",
+      name: "Register",
+      component: Register,
     },
     {
-      path: '/cart',
-      name: 'Cart',
-      component: Cart
-    }
-  ]
-})
+      path: "/cart",
+      name: "Cart",
+      component: Cart,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+  ],
+});
 
-export default router
+export default router;
